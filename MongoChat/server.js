7943208d1637f6a6495ca3a7c5373cea382569fc
Christian Seyoum <Chriscoding96@gmail.com>
2021-04-16 18:42:28 -0400

    chat with mongo

---
+++ MongoChat/server.js
@@ -0,0 +1,44 @@
+let app = require("express")()
+let http = require("http").Server(app)
+let io = require("socket.io")(http)
+
+var obj = require("mongoose")
+obj.Promise= global.Promise
+var url = "mongodb://localhost:27017/meanstack"
+var mongooseDbOption ={
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+obj.connect(url,mongooseDbOption)
+
+obj.connection; 
+
+var ChatSchema = obj.Schema({
+    name:String,
+    message:String
+})
+
+var chat = obj.model("",ChatSchema,"Chat")
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/index.html")
+})
+
+io.on("connection", (socket)=> {
+    console.log("Connected to Client")
+    socket.on("chat message", (msg)=>{
+        console.log(msg)
+        let c = new chat({
+            name:msg.split(",")[0],
+            message:msg.split(",")[1]
+        })
+        c.save((err,result)=>{
+            if(!err){
+                console.log(result)
+            }else {
+                console.log(err)
+            }
+        })
+    })
+})
+http.listen(9000, ()=>console.log("Runnin on 9000"))
